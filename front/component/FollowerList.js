0ebc2e35e7a85580dcc724d9e51426d423567453
ggipk <ggipk@naver.com>
2021-07-25 17:45:02 +0900

    프로필

---
+++ front/component/FollowerList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropType from 'prop-types'
+import {Button, Card, List} from 'antd';
+import {StopOutlined} from "@ant-design/icons";
+
+const FollowerList = ({header, data}) => {
+    return (
+        <List
+            style={{marginBottom: 20}}
+            grid={{gutter: 4, xs: 2, md: 3}}
+            size="small"
+            header={<div>{header}</div>}
+            loadMore={<div style={{textAlign: 'center', margin: '10px 0'}}><Button>더보기</Button></div>}
+            bordered
+            dataSource={data}
+            renderItem={(item) => (
+                <List.Item style={{marginTop: 20}}>
+                    <Card actions={[<StopOutlined key="stop"/>]}>
+                        <Card.Meta description={item.nickname}/>
+                    </Card>
+                </List.Item>
+            )}
+        />
+    );
+}
+
+FollowerList.propTyles = {
+    header:PropType.string.isRequired,
+    data:PropType.array.isRequired
+}
+export default FollowerList;
--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import AppLayout from "../component/AppLayout";
 
-const Profile = () => {
+import NicknameEditForm from "../component/NicknameEditForm";
+import FollowerList from "../component/FollowerList";
+import Head from "next/head";
 
+const Profile = () => {
+    const followerList = [{nickname:"횬쥬ㄴㄴ"},{nickname:"횬쥬ㄴ"},{nickname:"횬쥬ㅋ"}];
+    const followingList = [{nickname:"뽕쥬"},{nickname:"변쥬"},{nickname:"뽕주주"}];
     return (
-        <AppLayout>
-            <div>프로필 페이지</div>
-        </AppLayout>
+        <>
+            <Head>
+                <title>내 프로필 | NodeBird</title>
+            </Head>
+            <AppLayout>
+                <NicknameEditForm />
+                <FollowerList header="팔로잉 목록" data={followingList}/>
+                <FollowerList header="팔로워 목록" data={followerList}/>
+            </AppLayout>
+        </>
     );
 }
 
